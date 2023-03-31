9833ac4949577fcdbe1a6bb493a05573805ed250
Sofía <ravennasofia03@gmail.com>
2023-03-31 19:26:13 -0300

    Ejercicio parcialito

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    float nota, total=0, promedio;
+    int cantidad=0;
+
+    printf("El programa calculara el promedio de las notas ingresadas.\nPara finalizar ingrese -1.\n.\n");
+
+    do{
+        printf("Ingresar las notas: ");
+        scanf("%f",&nota);
+
+        if(nota != -1){
+            total=total+nota;
+            cantidad=cantidad+1;
+        }
+
+    }
+    while(nota != -1);
+    promedio=total/(cantidad);
+    printf("El promedio es: %f",promedio);
+
     return 0;
 }
